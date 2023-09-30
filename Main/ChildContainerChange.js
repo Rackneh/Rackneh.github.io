4f43f558ef1fdff1898e6aed08c9ad8e59bf75eb
Rackneh <steven.sparringa@hotmail.com>
2023-09-30 22:59:52 +0200

    Improving buttons on child pages

--- Main/ChildContainerChange.js
+++ Main/ChildContainerChange.js
@@ -1,11 +1,21 @@
+// CV Page buttons
 const ExperiencePage = document.getElementById("Ervaring");
 const SchoolPage = document.getElementById("Opleidingen");
 const SkillsPage = document.getElementById("Vaardigheden");
+// Projects page buttons
+const UGPPage = document.getElementById("UGP");
+const UGPIPage = document.getElementById("UGPI");
+const UGPAHPage = document.getElementById("UGPAH");
+// Works page buttons
+//lmao get to it homie
 
 function SwitchChildPage(switched) {
     ExperiencePage.classList.remove("active");
     SchoolPage.classList.remove("active");
     SkillsPage.classList.remove("active");
+    UGPPage.classList.remove("active");
+    UGPIPage.classList.remove("active");
+    UGPAHPage.classList.remove("active");
 
     switch (switched) 
     {
@@ -23,6 +33,21 @@ function SwitchChildPage(switched) {
         case "SkillsPage" :
         {
             SkillsPage.classList.add("active");
+            break;
+        }
+        case "UniversalGamePackagePage" :
+        {
+            UGPPage.classList.add("active");
+            break;
+        }
+        case "UGPInstallerPage" :
+        {
+            UGPIPage.classList.add("active");
+            break;
+        }
+        case "UGPAssetHandlerPage" :
+        {
+            UGPAHPage.classList.add("active");
             break;
         }
     }
